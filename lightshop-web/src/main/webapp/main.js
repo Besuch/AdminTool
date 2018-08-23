6ccf573b598037676b24b89cac602a6519024282
Denis <besuchg@gmail.com>
2018-08-23 03:45:08 +0300

    add simple html+js

--- lightshop-web/src/main/webapp/main.js
+++ lightshop-web/src/main/webapp/main.js
@@ -5,6 +5,7 @@ $('#btnGetUsers').click(function() {
     return false;
 });
 
+var parsedData;
 function getUsers() {
     console.log('findAll');
     $.ajax({
@@ -12,20 +13,43 @@ function getUsers() {
         url: rootURL + "/users",
         dataType: "json",
         success:function(data){
-            var parsedData = JSON.stringify(data[0]);
-            alert(parsedData);
+            parsedData = JSON.stringify(data[0]);
+            // alert(parsedData);
         }
     });
+    return parsedData;
 }
+
+var parsedData2;
+function getUsers2() {
+    console.log('findAll');
+    $.ajax({
+        type: 'GET',
+        url: rootURL + "/users",
+        dataType: "json",
+        success:function(data){
+            parsedData2 = data[0];
+            // alert(parsedData);
+        }
+    });
+    return parsedData2;
+}
+
 $(document).ready(function(){
     $("#btn1").click(function(){
         $("table").append("<tr>\n" +
-            "        <td align=\"center\" bgcolor=\"#9acd32\">Apple</td>\n" +
+            "        <td align=\"center\" bgcolor=\"#9acd32\">"+getUsers()+"</td>\n" +
             "        <td align=\"center\">5$</td>\n" +
             "    </tr>");
     });
 
     $("#btn2").click(function(){
         alert("button2");
+    });
+    $("#btn3").click(function(){
+        $("table").append("<tr>\n" +
+            "        <td align=\"center\" bgcolor=\"#9acd32\">"+getUsers2().firstName+"</td>\n" +
+            "        <td align=\"center\">5$</td>\n" +
+            "    </tr>");
     });
 });
